b2075d293de1a40e44802f079130a2ecfc738023
mr-dubik <mr.dubik.v@gmail.com>
2023-04-20 13:34:40 +0300

    feat: add login and register pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import RegisterPage from './Pages/RegisterPage/RegisterPage';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
+      <RegisterPage />
     </div>
   );
 }
