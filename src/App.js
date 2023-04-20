30485da0e679f6c8e0b6b772366e3afb80bf3ac7
mr-dubik <mr.dubik.v@gmail.com>
2023-04-20 13:41:06 +0300

    feat: clear app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import RegisterPage from './Pages/RegisterPage/RegisterPage';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
-      <RegisterPage />
     </div>
   );
 }
