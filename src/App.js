afc20f7d9784ebb884f1e87bb71c9a95817aed48
Liza Krestyaninova <liza0410200@gmail.com>
2023-04-28 23:45:12 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import Main from './Pages/MainPage/MainPage';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
-      <Main />
     </div>
   );
 }
